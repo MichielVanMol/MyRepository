6e17ccd1461e6d7f7dc75c6f90c3be8fef135016
MichielVanMol <michiel.van.mol@student.howest.be>
2017-09-26 11:35:31 +0200

    Test

---
+++ 01 Intro.py
@@ -0,0 +1,31 @@
+# Labo 01 - Basis Variabelen
+
+#print("Hello World, \t dag Johan")
+#print("1+1")
+
+#age = int(input("Give me your age "))
+#name = input("Give me your name ")
+#surname = input("Give me your surname ")
+
+#age += 1
+
+#print("\nVoornaam: {0}  \nAchternaam: {1} \n Leeftijd: {2: 10.2f}" .format(name, surname, age))
+
+#height = float(input("Give me the height of the triangle "))
+#base = float(input("Give me the base of the triangle "))
+#surface = base * height / 2
+
+#print("The surface of the triangle is: ", surface)
+
+#day = int(input("Give the amount of days "))
+#hour = int(input("Give the amount of hours "))
+#minute = int(input("Give the amount of minutes "))
+#second = int(input("Give the amount of seconds "))
+
+#totSec = (day * 86400) + (hour * 3600) + (minute * 60) + second
+
+#print("The total amount of seconds is: ", totSec)
+
+#seconds = int(input("Give me the amount of seconds "))
+
+#print("d:h:m:s -> ", days, ":",  hours, ":", minutes, ":", seconds)
